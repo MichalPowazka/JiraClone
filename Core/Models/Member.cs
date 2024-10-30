fd29914e3118b18212ee7b8e73311ce7d98045fa
michal <michal.powazka.workplace@gmail.com>
2024-10-30 20:59:01 +0100

    sadd

---
+++ Core/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Member : BaseEntity<long>
+    {
+
+      
+
+        //ProjectId
+       //UserId
+       //Lister
+       //usera
+    }
+
+
+}
--- Core/Models/Project.cs
+++ Core/Models/Project.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Common;
+using System.Text.Json.Serialization;
 
 namespace Core.Models;
 
@@ -7,8 +8,10 @@ public class Project : BaseEntity<int>
     public string? Name { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public List<Task> Tasks { get; set; } = [];
-    public List<Sprint> Sprints { get; set; }
+    public List<Task>? Tasks { get; set; } = [];
+    public List<Sprint>? Sprints { get; set; } = [];
+    public List<User>? Member  { get; set; } = [];
+
 }
 
 
